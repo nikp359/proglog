f9a5ff3ffa92ff07319e78eac91bca68fd7d991c
Nikita Popov <n.popov@space307.com>
2020-10-17 12:53:37 +0300

    add proto

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/nikp359/proglog
 go 1.14
 
 require (
+	github.com/gogo/protobuf v1.3.1
 	github.com/gorilla/mux v1.8.0
 	github.com/sirupsen/logrus v1.7.0
 )
--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
-	"log"
-
-	"github.com/nikp359/proglog/internal/server"
+	"fmt"
+	"time"
 )
 
 func main() {
-	srv := server.NewHTTPServer(":8080")
-	log.Fatal(srv.ListenAndServe())
+	ch1 := make(chan int)
+
+	for i := 0; i < 10; i++ {
+		go func(idx int) {
+			select {
+			case ch1 <- idx:
+				fmt.Println("Write to ch1")
+			default:
+				fmt.Println("skip")
+			}
+
+			ch1 <- idx
+		}(i)
+	}
+
+	input1 := <-ch1
+	input2 := <-ch1
+
+	fmt.Println(input1)
+	fmt.Println(input2)
+
+	// srv := server.NewHTTPServer(":8080")
+	// log.Fatal(srv.ListenAndServe())
+	time.Sleep(2 * time.Second)
 }
